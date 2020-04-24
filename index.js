22792d650c3219bf87efa11b09980eae563e87e4
Ashwin Vinod <ashwivinod16@gmail.com>
2020-04-24 17:45:55 +0530

    port added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors())
 app.use(bodyparser.json());
 
 app.use(bodyparser.urlencoded({extended: true}));
-app.set('port',process.env.Port)
+app.set('port',process.env.PORT)
 
 app.post('/urlsaving',function(req,res){
    
